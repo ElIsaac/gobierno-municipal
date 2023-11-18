7245ece22950c0c0ec6abcaf18403fa2268a7fdb
isaac ramirez <ramirezisaac.1518@gmail.com>
2023-11-17 19:41:51 -0700

    feat: archivos

--- menu.js
+++ menu.js
@@ -1,15 +1,15 @@
 import { cambiarPagina } from "./cambiarPagina.js";
 
-document.addEventListener("DOMContentLoaded", function() {
-    const menuNav = document.getElementById("menuNav");
+document.addEventListener("DOMContentLoaded", function () {
+  const menuNav = document.getElementById("menuNav");
 
-    // Asignar manejadores de eventos a los enlaces del menú
-    const links = menuNav.querySelectorAll("a");
-    links.forEach((link) => {
-        link.addEventListener("click", function(event) {
-            event.preventDefault(); // Evitar la acción por defecto del enlace
-            const pagina = link.id.replace("Link", ""); // Obtener el nombre de la página
-            cambiarPagina(pagina);
-        });
+  // Asignar manejadores de eventos a los enlaces del menú
+  const links = menuNav.querySelectorAll("a");
+  links.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault(); // Evitar la acción por defecto del enlace
+      const { id } = link;
+      cambiarPagina(id);
     });
+  });
 });
--- obtenerTemperatura.js
+++ obtenerTemperatura.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     let city = 'Ciudad Juárez';
-    let key = '5374d799563ffab1b478154715115401';
+    let key = 'a331e8c73d083e1d3b17c04e1a89d689';
     let temp = document.getElementById("temperatura");
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + key + '&units=metric')
         .then(response => response.json())
         .then(data => {
             temp.innerHTML = `<p>Temperatura: ${Math.round(data.main.temp)}°C <img src="./images/cloud.png" width="26" alt=""></p>`
+            console.log(data)
         })
         .catch(error => temp.innerHTML ='<p>Error al obtener los datos del clima</p>');
 });
