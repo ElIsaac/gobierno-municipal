6bed80b9a10e99543e4a1fb3e85cdf51e331294a
isaac ramirez <ramirezisaac.1518@gmail.com>
2023-11-22 12:50:05 -0700

    feat: paginas faltantes agregadas, audio y geolocalizacion agregados

--- index.js
+++ index.js
@@ -4,11 +4,31 @@ import "./menu.js";
 import "./obtenerTemperatura.js";
 
 var canvas = document.getElementById("miCanvas");
+var coordsFooter = document.getElementById('coords')
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight / 8;
 var context = canvas.getContext("2d");
 
 dibujarPoligono(context, canvas);
 cambiarPagina('inicio')
+function obtenerCoordenadas() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        function(position) {
+          const latitud = position.coords.latitude;
+          const longitud = position.coords.longitude;
+          document.getElementById("coords").innerHTML = `Latitud: ${latitud}, Longitud: ${longitud}`;
+        },
+        function(error) {
+          mostrarError();
+        }
+      );
+    } else {
+      alert("La geolocalización no es compatible con este navegador.");
+    }
+  }
 
-
+  function mostrarError() {
+    document.getElementById("coords").innerHTML = "Permiso denegado";
+  }
+obtenerCoordenadas()
